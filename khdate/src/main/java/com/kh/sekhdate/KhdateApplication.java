1a93d6ff3223b9431a599650bd410cc39a323ec6
yejin0104 <xpfl3487@naver.com>
2025-10-01 10:12:22 +0900

    최초 파일 업로드

---
+++ khdate/src/main/java/com/kh/sekhdate/KhdateApplication.java
@@ -0,0 +1,13 @@
+package com.kh.sekhdate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KhdateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KhdateApplication.class, args);
+	}
+
+}
---
+++ khdate/src/test/java/com/kh/sekhdate/KhdateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kh.sekhdate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KhdateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
